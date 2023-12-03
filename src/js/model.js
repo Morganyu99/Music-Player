902ee26999b1ba4aa09f3f1518f7105054f04059
Morgan <morganlaflay@outlook.com>
2023-12-03 16:54:42 +0530

    initial commit

---
+++ src/js/model.js
@@ -0,0 +1,14 @@
+const songs = [
+  {
+    name: "Lost in the city Lights",
+    artist: "Cosmo Sheldrake",
+    image: "/cover-1.png",
+    song: "/lost-in-city-lights-145038.mp3",
+  },
+  {
+    name: "Forest Lullaby",
+    artist: "Lesfm",
+    image: "/cover-2.png",
+    song: "/forest-lullaby-110624.mp3",
+  },
+];
