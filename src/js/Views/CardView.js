0b38b2724df935a3218cb3c221b9acd28045f714
Morgan <morganlaflay@outlook.com>
2023-12-05 16:32:25 +0530

    add playable functionality

--- src/js/Views/CardView.js
+++ src/js/Views/CardView.js
@@ -1,4 +1,7 @@
 import {
+  CALLED_BY_PROGRESSBAR,
+  LOADED,
+  LOADING,
   MAX_CONSIDERED_TIME,
   PAUSE_PLAY,
   PLAY_NEXT,
@@ -14,11 +17,9 @@ class CardView {
   _isPlaying = false;
   _startPlayer = document.querySelector(".card__player--start");
   _pausePlayer = document.querySelector(".card__player--pause");
+  _loaaded = false;
 
-  _changeinnerHTML(el, markup) {
-    this._parentElement.querySelector(`.${el}`).innerHTML = markup;
-  }
-
+  //InnerHTML and image load
   _loadImageElement(el, markup) {
     this._changeinnerHTML(el, markup);
     const childEl = this._parentElement.querySelector(`.${el}`).firstChild;
@@ -27,29 +28,121 @@ class CardView {
       childEl.classList.remove("hidden");
     });
   }
+  _changeinnerHTML(el, markup) {
+    this._parentElement.querySelector(`.${el}`).innerHTML = markup;
+  }
+  //Card Handler Method
 
   addCardHandler(handler) {
     //for progressBar
     const moveBar = (e) => {
       handler(PROGRESS_BAR_CLICKED, e);
     };
-    this._parentElement.addEventListener("click", function (e) {
+    this._parentElement.addEventListener("mousedown", function (e) {
       if (classListContains(e, "card__player--pause")) handler(PAUSE_PLAY);
       if (classListContains(e, "card__player--play-next")) handler(PLAY_NEXT);
       if (classListContains(e, "card__player--start")) handler(PAUSE_PLAY);
       if (classListContains(e, "card__player--play-back"))
         handler(PLAY_PREVIOUS);
       if (classListContains(e, "card__player--total-span")) {
-        e.target.addEventListener("mousedown", () => {
-          e.target.addEventListener("mousemove", moveBar);
-        });
-        e.target.addEventListener("mouseup", () => {
-          e.target.removeEventListener("mousemove", moveBar);
-        });
+        moveBar(e);
+        e.target.addEventListener("mousemove", moveBar);
+      }
+    });
+    this._parentElement.addEventListener("mouseup", function (e) {
+      if (classListContains(e, "card__player--total-span")) {
+        e.target.removeEventListener("mousemove", moveBar);
       }
     });
   }
-  progressBarChange(ev) {
+  //Rendering
+  renderSong(song) {
+    this._song = song;
+    this._generateMarkup();
+    this._loaaded = false;
+  }
+
+  _generateMarkup() {
+    this._loadImageElement(
+      "card__song-cover",
+      `<img src="./src/images${this._song.image}" alt="" class="hidden"/>`
+    );
+
+    this._changeinnerHTML(
+      "card__song-details",
+      `<h1 class="card__song-name">${this._song.name
+        .split(" ")
+        .map((n) => n[0].toUpperCase() + n.slice(1))
+        .join(" ")}</h1>
+          <h2 class="card__song-artist">${this._song.artist}</h2>`
+    );
+
+    this._musicElement.src = `./src/songs${this._song.song}`;
+
+    if (!this._loaaded) {
+      this._loaaded = LOADING;
+      this._musicElement.addEventListener(
+        "loadedmetadata",
+        this._playerProgress.bind(this)
+      );
+    }
+
+    this._musicElement.addEventListener(
+      "timeupdate",
+      this._playerProgress.bind(this)
+    );
+  }
+  _playerProgress(ev = "", calledByProgressBar = "") {
+    const currentTime = this._getDuration(this._musicElement.currentTime);
+    const endTime = this._getDuration(
+      this._musicElement.duration - this._musicElement.currentTime
+    );
+    if (calledByProgressBar === CALLED_BY_PROGRESSBAR) {
+      this._refreshPlayer(currentTime, endTime);
+      this.progressBarChange("", this._musicElement.currentTime);
+      return;
+    }
+    if (ev.type === "timeupdate") {
+      return;
+    }
+    this._updatePlayer(currentTime, endTime);
+    if (this._loaaded === LOADING) {
+      this._loaaded = LOADED;
+      this._musicElement.removeEventListener(
+        "loadedmetadata",
+        this._playerProgress
+      );
+    }
+  }
+  _getDuration(duration) {
+    const durationMinutes = String(Math.trunc(duration / 60));
+    const durationSeconds = String(
+      Math.trunc((duration / 60 - durationMinutes) * 60)
+    );
+    return `${durationMinutes.padStart(2, 0)}:${durationSeconds.padStart(
+      2,
+      0
+    )}`;
+  }
+  _updatePlayer(startTime, endTime) {
+    this._changeinnerHTML(
+      "card__player",
+      `<div class="card__time-stamps">
+          <span class="card--start-time">${startTime}</span>
+          <span class="card--end-time">${endTime}</span>
+        </div>
+    
+        <div class="card__player--total-span">
+          <div class="card__player--occ-span"></div>
+        </div>`
+    );
+  }
+
+  checkTime() {
+    return this._musicElement.currentTime > MAX_CONSIDERED_TIME;
+  }
+
+  progressBarChange(ev = "", currentTime) {
     const mainBar = this._parentElement.querySelector(
       ".card__player--total-span"
     );
@@ -57,8 +150,15 @@ class CardView {
       ".card__player--occ-span"
     );
     const mainBarSpec = mainBar.getBoundingClientRect();
-    const progress = ev.clientX - mainBarSpec.left;
-    progressBar.style.width = `${progress}px`;
+    const duration = this._musicElement.duration;
+    if (ev) {
+      const progress = ev.clientX - mainBarSpec.left;
+      progressBar.style.width = `${progress}px`;
+      this._changeCurrentTime((progress * duration) / mainBarSpec.width);
+      return;
+    }
+    const fillspan = (mainBarSpec.width / duration) * currentTime;
+    progressBar.style.width = `${fillspan}px`;
   }
 
   pausePlayPlayer() {
@@ -79,76 +179,25 @@ class CardView {
     this._isPlaying = true;
     this._musicElement.play();
   }
-  checkTime() {
-    return this._musicElement.currentTime > MAX_CONSIDERED_TIME;
-  }
+
   changeSongPlay() {
     this._isPlaying = true;
     this._startPlayer.classList.add("hidden");
     this._pausePlayer.classList.remove("hidden");
     this._musicElement.play();
   }
-  _updatePlayer() {
-    this._getDuration(this._musicElement.duration);
 
+  _refreshPlayer(startTime, endTime) {
     this._changeinnerHTML(
-      "card__player",
-      `<div class="card__time-stamps">
-          <span class="card--start-time">00:00</span>
-          <span class="card--end-time">${this._song.duration}</span>
-        </div>
-    
-        <div class="card__player--total-span">
-          <div class="card__player--occ-span"></div>
-        </div>`
+      "card__time-stamps",
+      `<span class="card--start-time">${startTime}</span>
+    <span class="card--end-time">${endTime}</span>`
     );
   }
 
-  _addProgressHandler(handler) {}
-  _getDuration(duration) {
-    const durationMinutes = String(Math.trunc(duration / 60));
-    const durationSeconds = String(
-      Math.trunc((duration / 60 - durationMinutes) * 60)
-    );
-    this._song.duration = `${durationMinutes.padStart(
-      2,
-      0
-    )}:${durationSeconds.padStart(2, 0)}`;
-  }
-
-  _generateMarkup() {
-    this._loadImageElement(
-      "card__song-cover",
-      `<img src="./src/images${this._song.image}" alt="" class="hidden"/>`
-    );
-
-    this._changeinnerHTML(
-      "card__song-details",
-      `<h1 class="card__song-name">${this._song.name
-        .split(" ")
-        .map((n) => n[0].toUpperCase() + n.slice(1))
-        .join(" ")}</h1>
-          <h2 class="card__song-artist">${this._song.artist}</h2>`
-    );
-
-    this._musicElement.src = `./src/songs${this._song.song}`;
-
-    this._musicElement.addEventListener(
-      "loadedmetadata",
-      this._updatePlayer.bind(this)
-    );
-  }
-
-  _durationChange() {
-    this._musicElement.addEventListener(
-      "durationchange",
-      this._updatePlayer.bind(this)
-    );
-  }
-
-  renderSong(song) {
-    this._song = song;
-    this._generateMarkup();
+  _changeCurrentTime(time) {
+    this._musicElement.currentTime = time;
+    this._playerProgress("", CALLED_BY_PROGRESSBAR);
   }
 }
 
--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,6 @@ export const PLAY_NEXT = 1;
 export const PLAY_PREVIOUS = -1;
 export const MAX_CONSIDERED_TIME = 60;
 export const PROGRESS_BAR_CLICKED = "clicked";
+export const CALLED_BY_PROGRESSBAR = "called";
+export const LOADING = "loading";
+export const LOADED = "loaded";
